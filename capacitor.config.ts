f12cd26ca9fdb352e2b427ceb30d00bd2405a331
Zagham Nadeem <bit.bytes@outlook.com>
2023-11-24 12:56:35 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionicnotes.dynamicisland',
+  appName: 'DynamicIsland',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
